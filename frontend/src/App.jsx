66515a9b36b0e597deb2cab1e6079aea70e53774
swoasti bhattacharjee <bhattacharjeeswoasti@gmail.com>
2025-10-22 18:44:50 +0530

    Routing of login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import SignUp from "./mainComponents/SignUp.jsx";
 import {BrowserRouter,Routes,Route} from "react-router-dom"
 import UserName from "./mainComponents/UserName.jsx";
+import Login from "./mainComponents/Login.jsx";
+
 const App=()=>{
   return (
     <BrowserRouter>
     <Routes>    
     <Route path="/" element={<SignUp />}/>
     <Route path="/UserName" element={<UserName/>}/> 
+    <Route path="/Login" element={<Login/>}/>
     </Routes>
     </BrowserRouter>
   );
