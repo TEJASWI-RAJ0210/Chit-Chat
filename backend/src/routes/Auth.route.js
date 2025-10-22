d02381e90e006474da24add6fb34eff487d1b234
Your Name <your-email@example.com>
2025-10-22 21:08:10 +0530

    Erorr in backend connection

--- backend/src/routes/Auth.route.js
+++ backend/src/routes/Auth.route.js
@@ -9,13 +9,13 @@ const JWT_SECRET = process.env.JWT_SECRET ;
 // Signup Page
 router.post('/signup', async (req, res) => {
     try {
-        const { username, email, fullName, avatar, password } = req.body;
+        const { username, email,password } = req.body;
         const existingUser = await User.findOne({ $or: [ { email }, { username } ] });
         if (existingUser) {
             return res.status(400).json({ message: 'User with this email or username already exists' });
         }
         const hashedPassword = await bcrypt.hash(password, 10);
-        const User = await User.create({ username, email, fullName, avatar, password: hashedPassword });
+        const User = await User.create({ username, email, password: hashedPassword });
         res.status(201).json({ message: 'User created successfully', userId: User._id });
     } catch (error) {
         res.status(500).json({ message: 'SignUp Failed' });
