c1c68796793a606e86a35ab78440028e4bc079a8
Your Name <your-email@example.com>
2025-12-26 11:51:05 +0530

    Updated Chat Header

--- frontend/src/chatComponents/chatHeader.jsx
+++ frontend/src/chatComponents/chatHeader.jsx
@@ -1,18 +1,23 @@
 import React from "react";
+import { Video,Phone,EllipsisVertical}  from "lucide-react";
 
 const ChatHeader = ({ chat }) => {
   return (
-    <div className="flex items-center gap-3 p-4 border-b bg-gray-700 text-white">
+    <div className="flex items-center gap-3 p-4 border-b bg-[#FFFFFF] text-white">
       <img
         src={chat?.avatar || `https://i.pravatar.cc/40?u=${chat?.id || "anon"}`}
         alt="avatar"
         className="w-10 h-10 rounded-full"
       />
-      <div className="flex-1">
-        <div className="font-semibold">{chat?.name || "Unknown"}</div>
-        {chat?.lastMessage && (
-          <div className="text-xs text-gray-300 truncate">{chat.lastMessage}</div>
-        )}
+      <div className="flex-1 ">
+        <div className="font-semibold text-[#393C43] font-Zen Kaku Gothic Antique">{chat?.name || "Unknown"}</div>
+         <div className="text-sm text-[#393C43]">Online</div>
+      </div>
+      <div className="flex items-center gap-4">
+         <Video className="text-[#393C43]" />
+         <Phone className="text-[#393C43]" />
+         <EllipsisVertical className="text-[#393C43]" />
+
       </div>
     </div>
   );
